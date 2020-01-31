1f55eababd89343afb017a87eadd6a628ab62f66
Dana Shacham <dshacham22@gmail.com>
2020-01-31 12:51:53 +0100

    try with active

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 (() => {
     const button = document.querySelector('.btn');
+    const ul = document.querySelector('.list-group');
+    const ulClasses = [...ul.classList];
 
     const toggleList = () => {
-        const ul = document.querySelector('.list-group');
-        const ulClasses = [...ul.classList];
-
-        if (ulClasses.includes('d-none')) {
-            ul.classList.replace('d-none', 'd-block');
-            button.textContent = 'Hide Destinations';
-        } else {
-            ul.classList.replace('d-block', 'd-none');
-            button.textContent = 'Show Destinations';
-        };
-    };
+        ul.classList.toggle('d-none');
+        ul.classList.toggle('d-block');
+        button.textContent = ul.classList.contains('d-none') ? 'Show Destinations' : 'Hide Destinations';
+    }
 
     button.addEventListener('click', toggleList, false);
+
+    const li = document.querySelectorAll('li');
+
+    const selectItem = function (event) {
+        this.classList.add('active');
+        this.classList.remove('active');
+        event.target.classList.add('active');
+    };
+
+    li.forEach(li => {
+        li.addEventListener('click', selectItem, false);
+    })
 })();
